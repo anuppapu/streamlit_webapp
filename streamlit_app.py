dc743aaa908b75d566e91de0524a3c353c9fd153
anuppapu <41812339+anuppapu@users.noreply.github.com>
2021-08-28 14:47:34 +0530

    Update streamlit_app.py script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
-st.beta_set_page_config(**PAGE_CONFIG)
 def main():
 	st.title("Awesome Streamlit for ML")
 	st.subheader("How to run streamlit from colab")
